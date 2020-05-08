637af4f4abeda8ca38177f45f97ec17612ab44b3
unknown <basupuja2007@gmail.com>
2020-05-08 14:04:53 +0200

    Sample Swagger intergration with .net core application

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SwaggerIntegration.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+        }
+
+        /// <summary>
+        /// Returns a range of integers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return Enumerable.Range(1, 5);
+        }
+    }
+}
